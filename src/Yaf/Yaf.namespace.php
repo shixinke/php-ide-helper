d1b7da1bd6980a6f04facfe55f16c5a063d82b6a
shixinke <ishixinke@qq.com>
2016-12-21 13:19:44 +0800

    增加yaf在开启命名空间后缺失的常量

---
+++ src/Yaf/Yaf.namespace.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* Yaf自动补全类(基于最新的3.0.4版本)
+* @author shixinke(http://www.shixinke.com)
+* @modified 2016/12/21
+*/
+
+/**
+*
+*/
+/**
+
+*/
+define('YAF\VERSION', '3.0.4');
+/**
+
+*/
+define('YAF\ENVIRON', 'dev');
+/**
+
+*/
+define('YAF\ERR\STARTUP_FAILED', 512);
+/**
+
+*/
+define('YAF\ERR\ROUTE_FAILED', 513);
+/**
+
+*/
+define('YAF\ERR\DISPATCH_FAILED', 514);
+/**
+
+*/
+define('YAF\ERR\AUTOLOAD_FAILED', 520);
+/**
+
+*/
+define('YAF\ERR\NOTFOUND\MODULE', 515);
+/**
+
+*/
+define('YAF\ERR\NOTFOUND\CONTROLLER', 516);
+/**
+
+*/
+define('YAF\ERR\NOTFOUND\ACTION', 517);
+/**
+
+*/
+define('YAF\ERR\NOTFOUND\VIEW', 518);
+/**
+
+*/
+define('YAF\ERR\CALL_FAILED', 519);
+/**
+
+*/
+define('YAF\ERR\TYPE_ERROR', 521);
